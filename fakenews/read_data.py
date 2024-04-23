cc16702dd668f3a592fa5010bc1e4d74e486ae99
danielbrito91 <danieloliveiradebrito@gmail.com>
2024-04-23 17:29:34 -0300

    eda fakerecogna + fakebr

---
+++ fakenews/read_data.py
@@ -0,0 +1,47 @@
+import glob
+import os
+import pathlib
+from typing import Literal
+
+import polars as pl
+
+
+def read_fakebr_corpus(
+    glob_path: Literal[
+        "data/fakebr-corpus/fake/*.txt",
+        "data/fakebr-corpus/true/*.txt",
+        "data/fakebr-corpus/size_normalized_texts/fake/*.txt",
+        "data/fakebr-corpus/size_normalized_texts/true/*.txt",
+    ],
+) -> pl.DataFrame:
+    """Leitura dos arquivos de texto do FakeBR Corpus.
+
+    https://github.com/roneysco/Fake.br-Corpus/tree/master
+
+    Args:
+        glob_path (Literal[str]): Local onde estão armazenados os
+        txt com as notícias.
+
+    Returns:
+        pl.DataFrame: Polars DataFrame contenedo textos, ids e label (`is_fake`: 0 para false, 1 para true)
+    """
+    txt_files = glob.glob(glob_path)
+    if "true" in glob_path:
+        is_fake = 0
+    else:
+        is_fake = 1
+
+    ids = []
+    texts = []
+    for file in txt_files:
+        file_name = os.path.basename(file)
+        id = (file_name.split(".")[0]) + f"-{is_fake}"
+        with open(file, "r") as f:
+            txt = f.read()
+
+        ids.append(id)
+        texts.append(txt)
+
+    return pl.DataFrame(dict(id=ids, texts=texts)).with_columns(
+        pl.lit(is_fake).alias("label")
+    )
